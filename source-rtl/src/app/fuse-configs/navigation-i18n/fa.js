f41c86674c3a6924f3c8eea5e6ec24330d5cc5bb
Alimohammadi20 <alimohi8118@gmail.com>
2023-08-26 16:04:48 +0330

    first commit

---
+++ source-rtl/src/app/fuse-configs/navigation-i18n/fa.js
@@ -0,0 +1,16 @@
+const locale = {
+  APPLICATIONS: 'اپلیکیشن ها',
+  DASHBOARDS: 'دشبرد ها',
+  CALENDAR: 'تقویم',
+  ECOMMERCE: 'ای کارمرس',
+  ACADEMY: 'اکادمی',
+  MAIL: 'ایمیل',
+  TODO: 'To-Do',
+  FILE_MANAGER: 'فایل منیجر',
+  CONTACTS: 'مخاطبین',
+  CHAT: 'چت',
+  SCRUMBOARD: 'اسکرام برد',
+  NOTES: 'یادداشت',
+};
+
+export default locale;
